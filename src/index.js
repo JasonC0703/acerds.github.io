629e23beeb03437dd29fa5e5038c641195003ae3
jason.chou <jason.chou@acer.com>
2021-03-23 02:18:00 +0800

    + ,

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-        <App />
+        <App />,
   document.getElementById('root')
 );
 
